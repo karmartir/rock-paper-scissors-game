d65de336a25b646e5c6e7e36262b715319fb6a3b
garen <karmartir@gmail.com>
2023-11-23 12:50:50 -0500

    v1

---
+++ src/Game.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const choices = ['🪨', '🧻', '🔪'];
+
+const Game = () => {
+    const [userChoice, setUserChoice] = useState(null);
+    const [computerChoice, setComputerChoice] = useState(null);
+    const [result, setResult] = useState(null);
+    const [winnerTool, setWinnerTool] = useState(null);
+    const [loserTool, setLoserTool] = useState(null);
+
+    const handleUserChoice = (choice) => {
+        const computerRandomChoice = choices[Math.floor(Math.random() * choices.length)];
+        setUserChoice(choice);
+        setComputerChoice(computerRandomChoice);
+        calculateWinner(choice, computerRandomChoice);
+    };
+
+    const calculateWinner = (user, computer) => {
+        if (user === computer) {
+            setResult("It's a tie!");
+        } else if (
+            (user === '🪨' && computer === '🔪') ||
+            (user === '🧻' && computer === '🪨') ||
+            (user === '🔪' && computer === '🧻')
+        ) {
+            setResult('You win!');
+            setWinnerTool(user);
+            setLoserTool(computer);
+        } else {
+            setResult('Computer wins!');
+            setWinnerTool(computer);
+            setLoserTool(user);
+        }
+    };
+
+    return (
+        <div className='main'>
+            <h1>Rock Paper Scissors Game</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                {choices.map((choice) => (
+                    <button
+                        key={choice}
+                        className='btn'
+                        onClick={() => handleUserChoice(choice)}
+                    >
+                        {choice}
+                    </button>
+                ))}
+            </div>
+            {userChoice && computerChoice && (
+                <div className='results'>
+                    <h2>Your choice: {userChoice}</h2>
+                    <h2>Computer's choice: {computerChoice}</h2>
+                    <h3>{result}</h3>
+                    <div>
+                        {result !== "It's a tie!" ?
+                        <div>
+                            {winnerTool && <span  className='span'>{winnerTool}</span>}
+                            {loserTool && <span className='span2'>{loserTool}</span>}
+                        </div>
+
+                     : null}
+                        </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Game;
